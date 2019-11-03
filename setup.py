d87f9d132c5da43164b2c9e00e9ff57c6c7b3836
Steven Loria <sloria1@gmail.com>
2019-11-03 12:23:51 -0500

    Test against Python 3.8

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     project_urls={
         "Issues": "https://github.com/sloria/environs/issues",
