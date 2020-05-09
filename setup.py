fe1f70fb5c8151b32297f0e417e46e413356a0a9
Steven Loria <sloria1@gmail.com>
2020-05-09 11:34:20 -0400

    Require Python 3.5.3 (#154)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ EXTRAS_REQUIRE = {
     "lint": ["flake8==3.7.9", "flake8-bugbear==20.1.4", "mypy==0.770", "pre-commit>=1.20,<3.0"],
 }
 EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["lint"] + ["tox"]
-PYTHON_REQUIRES = ">=3.5"
+PYTHON_REQUIRES = ">=3.5.3"
 
 
 def find_version(fname):
