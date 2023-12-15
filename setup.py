fa81d8967d03e8c862ce893746ea6f0b65c8946a
Steven Loria <sloria1@gmail.com>
2023-12-15 15:16:02 -0500

    Drop 3.7 in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ EXTRAS_REQUIRE = {
     "lint": ["flake8==4.0.1", "flake8-bugbear==21.9.2", "mypy==0.910", "pre-commit~=2.4"],
 }
 EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["lint"] + ["tox"]
-PYTHON_REQUIRES = ">=3.7"
+PYTHON_REQUIRES = ">=3.8"
 
 
 def find_version(fname):
@@ -55,7 +55,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
