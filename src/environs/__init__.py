646d5d60c11e6988b0d1c868f412030d5da635e6
Steven Loria <sloria1@gmail.com>
2025-01-07 15:49:17 -0500

    Move exceptions to their own module

--- src/environs/__init__.py
+++ src/environs/__init__.py
@@ -16,10 +16,15 @@ from pathlib import Path
 from urllib.parse import ParseResult, urlparse
 
 import marshmallow as ma
-from dj_database_url import DBConfig
 from dotenv.main import _walk_to_root, load_dotenv
 
 from . import fields
+from .exceptions import (
+    EnvError,
+    EnvSealedError,
+    EnvValidationError,
+    ParserConflictError,
+)
 from .types import (
     DictFieldMethod,
     EnumFuncMethod,
@@ -52,30 +57,6 @@ _EXPANDED_VAR_PATTERN = re.compile(r"(?<!\\)\$\{([A-Za-z0-9_]+)(:-[^\}:]*)?\}")
 
 # Reexport marshmallow's ValidationError. Custom validators should raise this for invalid input.
 ValidationError = ma.ValidationError
-
-
-class EnvError(ValueError):
-    """Raised when an environment variable or
-    if a required environment variable is unset.
-    """
-
-
-class EnvValidationError(EnvError):
-    """Raised when validation fails against one or all of the parsed environment variables."""
-
-    def __init__(self, message: str, error_messages: list[str] | ErrorMapping):
-        self.error_messages = error_messages
-        super().__init__(message)
-
-
-class EnvSealedError(TypeError, EnvError):
-    """Raised when trying to parse new values after an `Env` has already been sealed."""
-
-
-class ParserConflictError(ValueError):
-    """Raised when adding a custom parser that conflicts
-    with a built-in parser method.
-    """
 
 
 def _field2method(
