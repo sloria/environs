0583dd9958fbb0319ab12ee32c7af3d23a091181
Steven Loria <sloria1@gmail.com>
2025-01-16 15:19:17 -0500

    Typing fixes (#398)

--- src/environs/exceptions.py
+++ src/environs/exceptions.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from .types import ErrorMapping
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .types import ErrorMapping
 
 
 class EnvError(ValueError):
